beb31622ad939ca70fb90a4ca3f52063ed0ea03b
Compos Min <composmin@iinet.net.au>
2015-04-15 14:37:15 +1000

    Remove hard code to the ~/.aws/creds.tfvars file, so that we are prompted for credentials.

--- terraform/manifests/terra.sh
+++ terraform/manifests/terra.sh
@@ -5,7 +5,7 @@ function usage() {
 }
 
 if [ "X$1" == "Xget" -o "X$1" == "Xplan" -o "X$1" == "Xapply" -o "X$1" == "Xdestroy" ]; then
-    terraform $1 -var-file '~/.aws/childcap.tfvars' -var-file='values.tfvar' -var 'app_server_ami=ami-0b235e31' ${@:2}
+    terraform $1 -var-file='values.tfvar' -var 'app_server_ami=ami-0b235e31' ${@:2}
 else
     usage
 fi
